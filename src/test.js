cffd144743e67ea3d73e96700e4592004f346a69
Leo <st831209@gmail.com>
2020-09-03 18:34:29 +0800

    init測試

--- src/test.js
+++ src/test.js
@@ -4,9 +4,12 @@ import { increment } from './action'
 export default function Test() {
     //useSelectot 取得 store 的 state
     let counter = useSelector(state => state.conterReducer)
-    
+    //執行action
     let dispatch = useDispatch()
-
+    React.useEffect(()=>{
+        //執行 inline action
+         dispatch({type:'init',id:'10'})
+    },[])
     return (
         <>
             <p>Counter:{counter}</p>
